8a4fdc04a51361ecdbdcf785ab49d1e3dd6afa2d
Kaguenoakuma <shadowfocker666@gmail.com>
2025-01-07 22:09:50 -0600

    Proyecto base + implementacion del api context

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useContext } from "react"
+import BudgetForm from "./components/BudgetForm"
+import { BudgetContext } from "./context/BudgetContext"
+
+
+function App() {
+
+  const context = useContext(BudgetContext);
+
+  console.log(context);
+  
+  return (
+    <>
+      <header className="bg-blue-600 py-8 max-h-72">
+        <h1 className="uppercase text-center font-black text-4xl text-white">planificador de gastos</h1>
+      </header>
+      <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
+        <BudgetForm/>
+      </div>
+    </>
+  )
+}
+
+export default App
